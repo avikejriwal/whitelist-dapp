cdec8f4ac7781f6ad7ad9e84b9240903d8ea025c
Avi <avi.kejriwal@gmail.com>
2023-03-10 15:43:29 -0600

    nft app

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -2,5 +2,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xfFEfcC565eea3F5eDD17447e6fbE4098404d9c1B";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const NFT_CONTRACT_ADDRESS = "0x7F2929c99b39Bda884342C5F16A76842d02a9d26";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
