a8f2591a4f94c99d410a1ccd0b20aeb9c15ed1f5
Avi <avi.kejriwal@gmail.com>
2023-03-10 15:30:30 -0600

    add nft marketplace

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xfFEfcC565eea3F5eDD17447e6fbE4098404d9c1B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
